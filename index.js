57bd8a0eb858fe7867c2f4eebfe5f27ba0306489
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-05 12:51:39 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const interviews = require('./controllers/interviews');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to MindSet!')
+  res.send('Welcome to MindSet!');
 });
 
 app.listen(port, () => {
-  console.log(`MindSet server listening at http://localhost:${port}`)
+  console.log(`MindSet server listening at http://localhost:${port}`);
 });
