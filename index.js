de1b534ae8da8fa45ea7e2ff9a15534e1d20fbbe
nicolasblasco <nicoblasco96@gmail.com>
2021-11-05 20:48:06 -0300

    MS-27: Fix endpoint to create session

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const adminsController = require('./controllers/admins');
 const positionsController = require('./controllers/positions.js');
-const createController = require('./controllers/sessions');
+const sessionsController = require('./controllers/sessions.js');
 
 app.set('json spaces', 2);
 
@@ -20,6 +20,9 @@ app.get('/admins/byName/:name', adminsController.getByName);
 app.get('/positions', positionsController.getAll);
 app.get('/positions/byId/:id', positionsController.getById);
 app.get('/positions/byName/:name', positionsController.getByName);
+
+//SESSIONS
+app.get('/sessions/create', sessionsController.create);
 
 app.listen(port, () => {
   console.log(`MindSet server listening at http://localhost:${port}`);
