df94b0cba333227f148493d2af11f6810a05bdb3
Baltazar Cortina <baltazarcortina@gmail.com>
2021-11-05 15:39:08 -0300

    MS-02: Add endpoints to list all admins and a specific admin. Add json formatting

---
+++ controllers/admins.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const admins = JSON.parse(fs.readFileSync('./data/admins.json'));
+
+const getAll = (req, res) => {
+  res.json(admins);
+};
+
+const getById = (req, res) => {
+  const admin = admins.find(admin => admin.id === parseInt(req.params.id));
+  if (admin) {
+    res.json(admin);
+  } else {
+    res.send('User not found');
+  }
+}
+
+module.exports = {
+  getAll: getAll,
+  getById: getById
+};
--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const admins = require('./controllers/admins');
+
+app.set('json spaces', 2);
+
 app.get('/', (req, res) => {
   res.send('Welcome to MindSet!')
 });
+
+//ADMIN
+app.get('/admins/list', admins.getAll);
+app.get('/admins/list/:id', admins.getById);
 
 app.listen(port, () => {
   console.log(`MindSet server listening at http://localhost:${port}`)
