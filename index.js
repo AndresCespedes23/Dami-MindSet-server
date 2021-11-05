f93adcaab58e4267bdd8e64c75d4f2e621195f97
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-05 16:22:28 -0300

    MS-026: add callbacks routes in index

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Welcome to MindSet!')
 });
 
+// MS26-POSITIONS - List Positions
+const positionsM = require('./controllers/positions.js');
+
+app.get('/positions', positionsM.getAll );
+app.get('/positions/byId/:id', positionsM.getById);
+app.get('/positions/byName/:name', positionsM.getByName);
+
+
+
 app.listen(port, () => {
   console.log(`MindSet server listening at http://localhost:${port}`)
 });
