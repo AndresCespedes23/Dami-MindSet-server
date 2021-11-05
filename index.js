f2f918c80fea0e4bfe515092d747657ca3aadb41
Baltazar Cortina <baltazarcortina@gmail.com>
2021-11-04 21:29:17 -0300

    MS-00: Create basic express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to MindSet!')
+});
+
+app.listen(port, () => {
+  console.log(`MindSet server listening at http://localhost:${port}`)
+});
