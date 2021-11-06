bbe317a4f9f5194e9b19e0e6308ac38d5fd54b30
nicolasblasco <nicoblasco96@gmail.com>
2021-11-05 21:06:00 -0300

    MS-27: Add function to create session

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,2 +1,15 @@
 const fs = require('fs');
 const sessions = JSON.parse(fs.readFileSync('./data/sessions.json'));
+
+const create (req, res) => {
+    const newSession = {
+        id: req.query.id,
+        idPsychologist: req.query.idPsychologist,
+        idCandidate: req.query.idCandidate,
+        dateTime: req.query.dateTime,
+        status: req.query.status,
+        result: req.query.result
+    }
+    sessions.push(newSession);
+    res.json(sessions);
+}
