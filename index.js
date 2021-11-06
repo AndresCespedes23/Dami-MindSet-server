7afae7f2904d6d4c2707530220d8d82b963299b2
luisnannini <luisnannini97@gmail.com>
2021-11-05 22:34:27 -0300

    MS-15: move my js logic from index.js to profiles.js and edit the profiles.json

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const profilesController = require('./controllers/profiles.js');
+
+
+// PROFILES
+app.get('/profiles', profilesController.getAll);
+app.get('/profiles/create', profilesController.create);
 
 app.get('/', (req, res) => {
   res.send('Welcome to MindSet!')
