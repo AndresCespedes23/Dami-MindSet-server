2f0accdf519c0b3c4d4bd0eb88c2a0d0198a928e
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-06 19:49:39 -0300

    MS-07: add endpoint to index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const psychologistController = require('./controllers/psychologists');
 
 app.get('/', (req, res) => {
   res.send('Welcome to MindSet!');
 });
+
+//PSYCHOLOGISTS
+app.get('/psychologists', psychologistController.create);
+
 app.listen(port, () => {
   console.log(`MindSet server listening at http://localhost:${port}`);
 });
