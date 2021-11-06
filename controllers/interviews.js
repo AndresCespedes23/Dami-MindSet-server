cf4377f7320e1a0fb3724008d4193c5859d6a9b2
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-06 00:17:17 -0300

    implement create session function

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -1,2 +1,27 @@
+const fs = require('fs');
 
-  
+
+const create = (params) => {
+    const data = JSON.parse(fs.readFileSync('./data/interviews.json'));
+    let interview = {
+        id: params.id,
+        idCandidate: params.idCandidate,
+        idClient: params.idClient,
+        idPosition: params.idPosition,
+        date: params.date,
+        time: params.time,
+        status: params.status
+    }
+    data.push(interview);
+    fs.writeFile('./data/interviews.json', JSON.stringify(data), err => {
+        if(err) {
+            console.log(err);
+        }
+        return;
+    });
+    return interview;
+}
+
+module.exports = {
+    create: create,
+}
