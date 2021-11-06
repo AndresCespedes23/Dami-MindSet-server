c26b8b97c457f4a2067ed7737a64cf4d46efd6d2
luisnannini <luisnannini97@gmail.com>
2021-11-06 17:08:15 -0300

    MS-15: apply PR corrections

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,21 +1,22 @@
-//MS-15 - PROFILES - Create Profile
 const fs = require('fs');
 const profiles = JSON.parse(fs.readFileSync('./data/profiles.json'));
+
 // Get Profiles
 const getAll = (req, res) => {
     res.json(profiles);
 };
+
 // Create Profile
 const create = (req, res) => {
     const newProfile = {
         id: req.query.id,
         name: req.query.name,
         description: req.query.description
-    }
+    };
     profiles.push(newProfile);
     res.json(profiles);
 };
 module.exports = {
     getAll: getAll,
     create: create,
-  };
+};
