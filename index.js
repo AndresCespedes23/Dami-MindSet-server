494c1101431a5ce4fa73d7149114424b184d7fe8
luisnannini <luisnannini97@gmail.com>
2021-11-06 11:26:54 -0300

    MS-16: import last  index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const adminsController = require('./controllers/admins');
 const positionsController = require('./controllers/positions.js');
+const profilesController = require('./controllers/profiles.js');
+
 
 app.set('json spaces', 2);
 
@@ -19,6 +21,10 @@ app.get('/admins/byName/:name', adminsController.getByName);
 app.get('/positions', positionsController.getAll);
 app.get('/positions/byId/:id', positionsController.getById);
 app.get('/positions/byName/:name', positionsController.getByName);
+
+// PROFILES
+app.get('/profiles', profilesController.getAll);
+app.get('/profiles/create', profilesController.create);
 
 app.listen(port, () => {
   console.log(`MindSet server listening at http://localhost:${port}`);
