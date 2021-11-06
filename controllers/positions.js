55dc08089256fd52f187e695625e75504e647b40
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-06 16:02:34 -0300

    MS-26: optimize statusBoolean , delete blank lines, format ifCondition

--- controllers/positions.js
+++ controllers/positions.js
@@ -23,27 +23,26 @@ const getByName = (req, res) => {
 
 const create = (req, res) => {
     //check if all the parameters were sent
-    if (!req.query.id || !req.query.idClient || !req.query.idProfiles || !req.query.name || !req.query.description || !req.query.status || !req.query.address || !req.query.city || !req.query.postalCode){
+    if (!req.query.id 
+        || !req.query.idClient 
+        || !req.query.idProfiles 
+        || !req.query.name 
+        || !req.query.description 
+        || !req.query.status 
+        || !req.query.address 
+        || !req.query.city 
+        || !req.query.postalCode){
         res.send(400, {"Msg": "Some parameters are missing"});
     }
-
     // change the type of idProfiles from string to array of strings
     const idProfilesArray = req.query.idProfiles.split(",");
-    // change the type of status from string to boolean
-    let statusBoolean;
-    if (req.query.status === 'true'){
-        statusBoolean = true;
-    } else {
-        statusBoolean = false;
-    }
-    
     const newPosition = {
         id: req.query.id,
         idClient: req.query.idClient,
         idProfiles: idProfilesArray,
         name: req.query.name,
         description: req.query.description,
-        status: statusBoolean,
+        status: req.query.status === 'true',
         address: req.query.address,
         city: req.query.city,
         postalCode: req.query.postalCode
