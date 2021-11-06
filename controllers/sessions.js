e2a9660e0a2b6b67ca8411b681655d8e57ca5879
nicolasblasco <nicoblasco96@gmail.com>
2021-11-06 14:24:32 -0300

    MS-28: Add function to update session

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -14,6 +14,26 @@ const create = (req, res) => {
     res.json(sessions);
 };
 
+const update = (req, res) => {
+    const session = sessions.find(session => session.id === req.params.id);
+    if(session) {
+        const updateSession = {
+            id: req.query.id,
+            idPsychologist: req.query.idPsychologist,
+            idCandidate: req.query.idCandidate,
+            dateTime: req.query.dateTime,
+            status: req.query.status,
+            result: req.query.result
+        }
+        session.idPsychologist = updateSession.idPsychologist ? updateSession.idPsychologist : session.idPsychologist;
+        session.idCandidate = updateSession.idCandidate ? updateSession.idCandidate : session.idCandidate;
+        session.dateTime = updateSession.dateTime ? updateSession.dateTime : session.dateTime;
+        session.status = updateSession.status ? updateSession.status : session.status;
+        session.result = updateSession.result ? updateSession.result : session.result;
+        res.json({ msg: 'Session updated', session});
+    } 
+};
+
 module.exports = {
     create: create
 };
