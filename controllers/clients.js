f8e7487c8fa1cf1e7804ed8127fb01f34ce57b97
Agustin Macazzaga <ag.macazz@gmail.com>
2021-11-06 18:04:50 -0300

    MS-11 list client working properly

---
+++ controllers/clients.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const clients = JSON.parse(fs.readFileSync('./data/clients.json'));
+//CLIENT LIST///
+const getAll = (req, res) => {
+  res.json(clients);
+};
+
+const getById = (req, res) => {
+  const client = clients.find(client => client.id == req.params.id);
+  if (client) {
+    res.json(client);
+  } else {
+    res.send('Client not found');
+  }
+};
+
+const getByName = (req, res) => {
+  const client = clients.find(client => client.name === req.params.name);
+  if (client) {
+    res.json(client);
+  } else {
+    res.send('Client not found');
+  }
+}
+  const getByphoneNumber = (req, res) => {
+    const client = clients.find(client => client.phoneNumber === req.params.phoneNumber);
+    if (client) {
+      res.json(client);
+    } else {
+      res.send('Client not found');
+    }
+};
+const getByCuit = (req, res) => {
+  const client = clients.find(client => client.cuit === req.params.cuit);
+  if (client) {
+    res.json(client);
+  } else {
+    res.send('Client not found');
+  }
+};
+const getByAddress = (req, res) => {
+  const client = clients.find(client => client.address === req.params.address);
+  if (client) {
+    res.json(client);
+  } else {
+    res.send('Client not found');
+  }
+};
+const getByActivity = (req, res) => {
+  const client = clients.find(client => client.activity === req.params.activity);
+  if (client) {
+    res.json(client);
+  } else {
+    res.send('Client not found');
+  }
+};
+//CLIENT//UPDATE
+/*const editAdmin = (req, res) => {
+  const adminIndex = Admins.findIndex((item) => item.id === req.query.id)
+  if (adminIndex !== -1) {
+    Admins[adminIndex] = {
+      id: Admins[adminIndex].id,
+      first_name: req.query.first_name || Admins[adminIndex].first_name,
+      last_name: req.query.last_name || Admins[adminIndex].last_name,
+      user_name: req.query.user_name || Admins[adminIndex].user_name,
+      email: req.query.email || Admins[adminIndex].email,
+      password: req.query.password || Admins[adminIndex].password,
+    }
+    fs.writeFile('./data/admins.json', JSON.stringify(Admins), {}, (error) => {
+      if (error) {
+        res.status(400).send(error)
+      } else {
+        res.status(201).json(Admins[adminIndex])
+      }
+    })
+  } else {
+    res.status(404).send("Admin not found")
+  }}*/
+
+module.exports = {
+  getAll: getAll,
+  getById: getById,
+  getByName: getByName,
+  getByphoneNumber:getByphoneNumber,
+  getByCuit:getByCuit,
+  getByAddress:getByAddress,
+  getByActivity:getByActivity
+
+};
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const clientsController = require('./controllers/clients');
 
 app.get("/", (req, res) => {
-  res.send("Welcome to MindSet!");
+  res.send("Welcome to MindSet!!!");
 });
+
+//CLIENTS//LIST
+app.get('/clients', clientsController.getAll);
+app.get('/clients/byId/:id', clientsController.getById);
+app.get('/clients/byName/:name', clientsController.getByName);
+app.get('/clients/byphoneNumber/:phoneNumber', clientsController.getByphoneNumber);
+app.get('/clients/byCuit/:cuit', clientsController.getByCuit);
+app.get('/clients/byAddress/:address', clientsController.getByAddress);
+app.get('/clients/byActivity/:activity', clientsController.getByActivity);
+//CLIENTS//UPDATE
+
 
 app.listen(port, () => {
   console.log(`MindSet server listening at http://localhost:${port}`);
