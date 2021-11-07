6570af526299823b42f4af5362c4569d41080830
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-07 16:47:21 -0300

    MS-05: delete extra spaces

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -4,15 +4,15 @@ const candidates = JSON.parse(fs.readFileSync('./data/candidates.json'));
 // MS-05: remove candidates
 
 const remove = (req, res) => {
-    const candidate = candidates.find(candidate => candidate.id === req.params.id);
-    if (candidate) {
-      candidates.splice(candidate, 1);
-      res.json(candidates);
-    } else {
-      res.send('User not removed');
-    }
-  };
-  
-  module.exports = {
-      remove: remove
+  const candidate = candidates.find(candidate => candidate.id === req.params.id);
+  if (candidate) {
+    candidates.splice(candidate, 1);
+    res.json(candidates);
+  } else {
+    res.send('User not removed');
   }
+};
+  
+module.exports = {
+  remove: remove
+}
