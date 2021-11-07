40382bccd5e5be5a68fd08d123be6cef24aecf64
luisnannini <luisnannini97@gmail.com>
2021-11-07 12:11:04 -0300

    MS-16: Update Profiles controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,33 +1,24 @@
-//MS-15 - PROFILES - Create Profile
 const fs = require('fs');
 const profiles = JSON.parse(fs.readFileSync('./data/profiles.json'));
+
 // Get Profiles
 const getAll = (req, res) => {
     res.json(profiles);
 };
+
 // Create Profile
 const create = (req, res) => {
     const newProfile = {
-        id: req.query.id,
+        id: profiles.length + 1,
         name: req.query.name,
         description: req.query.description
-    }
+    };
     profiles.push(newProfile);
     res.json(profiles);
 };
 
-
-//MS-16 - PROFILES - Update Profiles
-const update = (req, res) => {
-    const idProfile = 
-
-
-    res.json(profiles);
-};
-
-// MODULE EXPORTS
+// Module Exports
 module.exports = {
     getAll: getAll,
     create: create,
-    update: update,
 };
