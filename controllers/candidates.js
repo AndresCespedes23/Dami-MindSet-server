737183cd4f3e80c1422170d5f1c5139131382a42
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-07 16:02:17 -0300

    MS-03: Delete req.query.id & req.query.profiles

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -5,7 +5,6 @@ const candidates = JSON.parse(fs.readFileSync('./data/candidates.json'));
 
 const create = (req, res) => {
     const newCandidate = {
-        id: req.query.id,
         name: req.query.name,
         email: req.query.email,
         gender: req.query.gender,
@@ -19,7 +18,6 @@ const create = (req, res) => {
         country: req.query.country,
         timeRange: req.query.timeRange,
         status: req.query.status,
-        profiles: req.query.profiles,
         username: req.query.username,
         password: req.query.password
     }
