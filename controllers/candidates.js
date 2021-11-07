afcd2d69eb09643c2974e45a3e6be1d4ee11b655
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-07 19:36:57 -0300

    MS-04: Change code to resolve update problems

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -1,40 +1,33 @@
 const fs = require('fs');
-const candidates = JSON.parse(fs.readFileSync('./data/candidates.json'));
+let candidates = JSON.parse(fs.readFileSync('./data/candidates.json'));
 
 // MS-04: update candidates
 
 const update = (req, res) => {
-    const candidate = candidates.find(candidate => candidate.id === req.params.id);
-    const index = candidates.indexOf(candidate);
-    if (candidate) {
-      req.query.id !== null ? candidates[index].id = req.query.id : candidates[index].id;
-      req.query.name !== null ? candidates[index].name = req.query.name : candidates[index].name;
-      req.query.email !== null ? candidates[index].email = req.query.email: candidates[index].email;
-      // candidates[index] = {
-      //     id: req.query.id,
-      //     name: req.query.name,
-      //     email: req.query.email,
-      //     gender: req.query.gender,
-      //     address: req.query.address,
-      //     phoneNumber: req.query.phoneNumber,
-      //     dni: req.query.dni,
-      //     dateOfBirth: req.query.dateOfBirth,
-      //     zipCode: req.query.zipCode,
-      //     city: req.query.city,
-      //     state: req.query.state,
-      //     country: req.query.country,
-      //     timeRange: req.query.timeRange,
-      //     status: req.query.status,
-      //     profiles: req.query.profiles,
-      //     username: req.query.username,
-      //     password: req.query.password
-      // }
-      res.json(candidates);
-    } else {
-      res.send('User not updated');
-    }
-  };
-
-  module.exports = {
-    update: update
+  const candidate = candidates.find(candidate => candidate.id === req.params.id);
+  const index = candidates.findIndex(candidate => candidate.id === req.params.id);
+  if (candidate) {
+      candidate.name = req.query.name ? req.query.name : candidate.name,
+      candidate.email = req.query.email ? req.query.email : candidate.email,
+      candidate.gender = req.query.gender ? req.query.gender : candidate.gender,
+      candidate.address = req.query.address ? req.query.address : candidate.address,
+      candidate.phoneNumber = req.query.phoneNumber ? req.query.phoneNumber : candidate.phoneNumber,
+      candidate.dni = req.query.dni ? req.query.dni : candidate.dni,
+      candidate.dateOfBirth  = req.query.dateOfBirth ? req.query.dateOfBirth : candidate.dateOfBirth,
+      candidate.city = req.query.city ? req.query.city : candidate.city,
+      candidate.state = req.query.state ? req.query.state : candidate.state,
+      candidate.country = req.query.country ? req.query.country : candidate.country,
+      candidate.timeRange = req.query.timeRange ? req.query.timeRange : candidate.timeRange,
+      candidate.status = req.query.status ? req.query.status : candidate.status,
+      candidate.username = req.query.username ? req.query.username : candidate.username,
+      candidate.password = req.query.password ? req.query.password : candidate.password
+    candidates[index] = candidate;
+    res.json(candidates);
+  } else {
+    res.send('User not updated');
   }
+};
+
+module.exports = {
+  update: update
+}
