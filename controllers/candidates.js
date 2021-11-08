7b1715cda3a366c631a09fa58b7fec70ae3e2ff5
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-07 21:06:30 -0300

    MS-05: Add index to assign new ids to new candidates

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -4,23 +4,25 @@ let candidates = JSON.parse(fs.readFileSync('./data/candidates.json'));
 // MS-03: create candidates
 
 const create = (req, res) => {
+  const index = candidates.length;
   const newCandidate = {
-      name: req.query.name,
-      email: req.query.email,
-      gender: req.query.gender,
-      address: req.query.address,
-      phoneNumber: req.query.phoneNumber,
-      dni: req.query.dni,
-      dateOfBirth: req.query.dateOfBirth,
-      zipCode: req.query.zipCode,
-      city: req.query.city,
-      state: req.query.state,
-      country: req.query.country,
-      timeRange: req.query.timeRange,
-      status: req.query.status,
-      username: req.query.username,
-      password: req.query.password
-  }
+    id: index + 1,
+    name: req.query.name,
+    email: req.query.email,
+    gender: req.query.gender,
+    address: req.query.address,
+    phoneNumber: req.query.phoneNumber,
+    dni: req.query.dni,
+    dateOfBirth: req.query.dateOfBirth,
+    zipCode: req.query.zipCode,
+    city: req.query.city,
+    state: req.query.state,
+    country: req.query.country,
+    timeRange: req.query.timeRange,
+    status: req.query.status,
+    username: req.query.username,
+    password: req.query.password
+}
   candidates.push(newCandidate);
   res.json(candidates);
 };
@@ -36,7 +38,7 @@ const remove = (req, res) => {
   } else {
     res.send('User not removed');
   }
-}
+};
 
 // MS-06: list candidates
 
