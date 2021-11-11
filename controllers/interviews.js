730e0dd0fe3fbc6cf474ce2506b9192150d61c53
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-11 20:55:20 -0300

    MS-38: Fix indentation in interviews.js & interviews.json files to 2 spaces

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -2,53 +2,53 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./data/interviews.json'));
 
 const create = (req,res) => {
-    let id = null;
-    do {
-        id = `${Math.round(Math.random()*10000)}`;
-    } while (data.find(interview => interview.id === id));
-    const interview = {
-        id: id,
-        idCandidate: req.query.idCandidate,
-        idClient: req.query.idClient,
-        idPosition: req.query.idPosition,
-        date: req.query.date,
-        time: req.query.time,
-        status: req.query.status
-    }
-    data.push(interview);
-    res.send(`Interview succesfully created! ${JSON.stringify(interview)}`);
+  let id = null;
+  do {
+      id = `${Math.round(Math.random()*10000)}`;
+  } while (data.find(interview => interview.id === id));
+  const interview = {
+      id: id,
+      idCandidate: req.query.idCandidate,
+      idClient: req.query.idClient,
+      idPosition: req.query.idPosition,
+      date: req.query.date,
+      time: req.query.time,
+      status: req.query.status
+  }
+  data.push(interview);
+  res.send(`Interview succesfully created! ${JSON.stringify(interview)}`);
 }
 
 const update = (req,res) => {
-    const index = data.findIndex(interview => req.params.id === interview.id);
-    if(index === -1) res.send('Could not find interview with specified ID');
-    for(property in req.query) {
-        data[index][property] = req.query[property];
-    }
-    res.send(`Interview succesfully updated! ${JSON.stringify(data[index])}`);
+  const index = data.findIndex(interview => req.params.id === interview.id);
+  if(index === -1) res.send('Could not find interview with specified ID');
+  for(property in req.query) {
+      data[index][property] = req.query[property];
+  }
+  res.send(`Interview succesfully updated! ${JSON.stringify(data[index])}`);
 }
 
 const remove = (req, res) => {
-    const index = data.findIndex(interview => req.params.id === interview.id);
-    if(index === -1) res.send('Could not find interview with specified ID');
-    const cancelledInterview = data.splice(index,1);
-    res.send(`Interview cancelled! ${JSON.stringify(cancelledInterview)}`);
+  const index = data.findIndex(interview => req.params.id === interview.id);
+  if(index === -1) res.send('Could not find interview with specified ID');
+  const cancelledInterview = data.splice(index,1);
+  res.send(`Interview cancelled! ${JSON.stringify(cancelledInterview)}`);
 }
 
 const getAll = (req, res) => {
-    res.json(data);
+  res.json(data);
 }
 
 const getById = (req, res) => {
-    const index = data.findIndex(interview => req.params.id === interview.id);
-    if(index === -1) res.send('Could not find interview with specified ID');
-    res.json(data[index]);
+  const index = data.findIndex(interview => req.params.id === interview.id);
+  if(index === -1) res.send('Could not find interview with specified ID');
+  res.json(data[index]);
 }
 
 module.exports = {
-    create: create,
-    update: update,
-    remove: remove,
-    getAll: getAll,
-    getById: getById
+  create: create,
+  update: update,
+  remove: remove,
+  getAll: getAll,
+  getById: getById
 }
