18cad9651ce553bb3b76ecd0fd75880273ee83c0
luisnannini <luisnannini97@gmail.com>
2021-11-11 14:15:15 -0300

    MS-39: add res.status error to getAlres.

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,21 +3,21 @@ const profiles = JSON.parse(fs.readFileSync('./data/profiles.json'));
 
 // Get Profiles
 const getAll = (req, res) => {
-  res.json(profiles);
-};
+  return res.status(200).json(profiles);
+}
 
 // Create Profile
 const create = (req, res) => {
   if (!req.query.id || !req.query.name || !req.query.description) {
     return res.status(400).send("Some parameters are missing");
-  };
+  }
   const newProfile = {
     id: (profiles.length + 1).toString(),
     name: req.query.name,
     description: req.query.description
   };
   return res.status(201).json(newProfile);
-};
+}
 
 // Update Profile
 const update = (req, res) => {
@@ -31,7 +31,7 @@ const update = (req, res) => {
     return res.status(200).json(selectedProfile);
   };
   return res.status(404).send("Error: profile does not exist");
-};
+}
 
 // Remove Profile
 const remove = (req, res) => {
@@ -42,7 +42,7 @@ const remove = (req, res) => {
     return res.status(200).send("Profile removed");
   }
   return res.status(404).send("Error: profile does not exist, nothing removed");
-};
+}
 
 // Module Exports
 module.exports = {
