3051befc16c30ee3b40b36dbd1a46a525ceb596e
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-11 18:55:30 -0300

    MS-34: added http codes responses, add parameters validation on create, remove else statemens, remove trailing spaces

--- controllers/psychologists.js
+++ controllers/psychologists.js
@@ -1,90 +1,87 @@
 const fs = require('fs')
 const psychologists = JSON.parse(fs.readFileSync('./data/psychologists.json'));
 
-//MS 07 Create Psychologists
-
 const create = (req, res) => {
-    const newPsychologist = {
-        id: (psychologists.length + 1).toString(),
-        name: req.query.name,
-        email: req.query.email,
-        userName: req.query.userName,
-        passWord: req.query.passWord,
-        phoneNumber: req.query.phoneNumber,
-        enrollmentNumber: req.query.enrollmentNumber,
-        status: req.query.status,
-        timeRange: req.query.timeRange,
-        toTimeRange: req.query.toTimeRange,
-        dayRange: req.query.dayRange,
-    }
-    psychologists.push(newPsychologist);
-    res.json(psychologists);
+  if (!req.query.id
+    || !req.query.name
+    || !req.query.email
+    || !req.query.userName // **********************************************************
+    || !req.query.password
+    || !req.query.phoneNumber
+    || !req.query.enrollmentNumber
+    || !req.query.status
+    || !req.query.timeRange
+    || !req.query.toTimeRange
+    || !req.query.dayRange) {
+      return res.status(400).json({ "Msg": "Some parameters are missing" });
+  }
+  const newPsychologist = {
+    id: (psychologists.length + 1).toString(),
+    name: req.query.name,
+    email: req.query.email,
+    userName: req.query.userName,
+    password: req.query.password,
+    phoneNumber: req.query.phoneNumber,
+    enrollmentNumber: req.query.enrollmentNumber,
+    status: req.query.status === 'true',
+    timeRange: req.query.timeRange,
+    toTimeRange: req.query.toTimeRange,
+    dayRange: req.query.dayRange,
+  }
+  psychologists.push(newPsychologist);
+  res.status(201).json(newPsychologist);
 };
-
-
-//MS 08 update psychologists
 
 const update = (req, res) => {
-    const psychologist = psychologists.find(psychologist => psychologist.id === req.params.id);
-    const index = psychologists.findIndex(psychologist => psychologist.id === req.params.id);
-    if(psychologist) {
-        psychologist.name = req.query.name ? req.query.name : psychologist.name,
-        psychologist.email = req.query.email ? req.query.email : psychologist.email,
-        psychologist.userName = req.query.userName ? req.query.userName : psychologist.userName,
-        psychologist.passWord = req.query.passWord ? req.query.passWord : psychologist.passWord,
-        psychologist.phoneNumber = req.query.phoneNumber ? req.query.phoneNumber : psychologist.phoneNumber,
-        psychologist.enrollmentNumber = req.query.enrollmentNumber ? req.query.enrollmentNumber : psychologist.enrollmentNumber,
-        psychologists[index] = psychologist;
-        res.json(psychologist);
-    }
-        res.send('Psychologist Not Updated');
+  const psychologist = psychologists.find(psychologist => psychologist.id === req.params.id);
+  const index = psychologists.findIndex(psychologist => psychologist.id === req.params.id);
+  if (psychologist) {
+    psychologist.name = req.query.name ? req.query.name : psychologist.name,
+    psychologist.email = req.query.email ? req.query.email : psychologist.email,
+    psychologist.userName = req.query.userName ? req.query.userName : psychologist.userName,
+    psychologist.password = req.query.password ? req.query.password : psychologist.password,
+    psychologist.phoneNumber = req.query.phoneNumber ? req.query.phoneNumber : psychologist.phoneNumber,      psychologist.enrollmentNumber = req.query.enrollmentNumber ? req.query.enrollmentNumber : psychologist.enrollmentNumber,
+    psychologists[index] = psychologist;
+    return res.status(201).json(psychologists[index]);
+  }
+  res.status(404).json({ "Msg": "Psychologist with that ID does not exist" });
 };
-
-
-// MS 09 Remove Psychologists
 
 const remove = (req, res) => {
-    const psychologist = psychologists.find(psychologist => psychologist.id === req.params.id);
-    if(psychologist) {
-        const psychFilter = psychologists.filter(psychologist => psychologist.id !== req.params.id);
-        psychologists = psychFilter;
-        res.json(psychologists);
-    } else {
-        res.send('Psychologists not removed');
-    }
+  const psychologist = psychologists.find(psychologist => psychologist.id === req.params.id);
+  if (psychologist) {
+    const psychFilter = psychologists.filter(psychologist => psychologist.id !== req.params.id);
+    psychologists = psychFilter;
+    return res.status(200).json(psychologist);
+  }
+  res.status(404).json({ "Msg": "Psychologist with that ID does not exist" });
 };
-
-
-// MS 10 Lists Psychologists
 
 const getAll = (req, res) => {
-    res.json(psychologists)
+  res.status(200).json(psychologists);
 }
+
 const getById = (req, res) => {
-    const psychologist = psychologists.find(psychologist => psychologist.id === req.params.id);
-
-    if(psychologist) {
-        res.json(psychologist);
-    } else {
-        res.status(400).send('Psychologist not Found');
-    }
+  const psychologist = psychologists.find(psychologist => psychologist.id === req.params.id);
+  if (psychologist) {
+    return res.status(200).json(psychologist);
+  }
+  res.status(404).send({ "Msg": "Psychologist not Found" });
 };
+
 const getByName = (req, res) => {
-    const psychologist = psychologists.find(psychologist => psychologist.name === req.param.name);
-
-    if(psychologist) {
-        res.json(psychologist);
-    } else {
-        res.status(400).send('Psychologist not found');
-    }
+  const psychologist = psychologists.find(psychologist => psychologist.name === req.param.name);
+  if (psychologist) {
+    return res.status(200).json(psychologist);
+  }
+  res.status(404).send({ "Msg": "Psychologist not Found" });
 };
-
 
 module.exports = {
-    create: create,
-    update: update,
-    getAll: getAll,
-    getById: getById,
-    getByName: getByName,
-    remove: remove,
+  create: create,
+  update: update,
+  getAll: getAll,
+  getById: getById,
+  getByName: getByName,
+  remove: remove,
 };
