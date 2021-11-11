7e7a24c64bbb65acd0cf841945ecc06fcb2920b0
Agustin Macazzaga <ag.macazz@gmail.com>
2021-11-11 18:35:38 -0300

    MS-42 return on update

--- controllers/admins.js
+++ controllers/admins.js
@@ -5,41 +5,34 @@ const getAll = (req, res) => {
   return res.status(200).json(admins)
   };
 
-  const getById = (req, res) => {
-    const admin = admins.find(admin => admin.id === req.params.id);
-    if (admin) return res.status(200).json(admin);
-    return res.status(404).send("Admin not found");
-    }
+const getById = (req, res) => {
+  const admin = admins.find(admin => admin.id === req.params.id);
+  if (admin) return res.status(200).json(admin);
+  return res.status(404).send("Admin not found");
+  }
 
 const getByName = (req, res) => {
   const admin = admins.find(admin => admin.name === req.params.name);
-
-  if (admin) {
-    res.json(admin);
-  } else {
-    res.status(400).send('Admin not found');
+  if (admin) return res.status(200).json(admin);
+  return res.status(404).send("Admin not found");
   }
-};
+
 
 const update = (req, res) => {
   const found = admins.some(admin => admin.id === req.params.id);
-
   if (found) {
-    const updateAdmin = req.query;
-    admins.forEach(admin => {
-      if (admin.id === req.params.id) {
-        admin.name = updateAdmin.name ? updateAdmin.name : admin.name;
-        admin.email = updateAdmin.email ? updateAdmin.email : admin.email;
-        admin.username = updateAdmin.username ? updateAdmin.username : admin.username;
-        admin.password = updateAdmin.password ? updateAdmin.password : admin.password;
-
-        res.json(admin);
-      }
-    });
-  } else {
-    res.status(400).send('Admin not found');
-  }
-};
+  const updateAdmin = req.query;
+  admins.forEach(admin => {
+    if (admin.id === req.params.id) {
+      admin.name = updateAdmin.name ? updateAdmin.name : admin.name;
+      admin.email = updateAdmin.email ? updateAdmin.email : admin.email;
+      admin.username = updateAdmin.username ? updateAdmin.username : admin.username;
+      admin.password = updateAdmin.password ? updateAdmin.password : admin.password;
+      return res.status(200).json(admin);
+    }
+  });
+  } 	return res.status(404).send("Admin not found");
+  };
 
 module.exports = {
   getAll: getAll,
