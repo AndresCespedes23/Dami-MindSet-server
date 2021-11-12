90649741899eddf317e648424c26526667b2e34a
Agustin Macazzaga <ag.macazz@gmail.com>
2021-11-12 10:35:17 -0300

    MS-42 indentation fix

--- controllers/admins.js
+++ controllers/admins.js
@@ -3,40 +3,41 @@ const admins = JSON.parse(fs.readFileSync('./data/admins.json'));
 
 const getAll = (req, res) => {
   return res.status(200).json(admins);
-  };
+}
 
 const getById = (req, res) => {
   const admin = admins.find(admin => admin.id === req.params.id);
   if (admin) return res.status(200).json(admin);
   return res.status(404).send("Admin not found");
-  }
+}
 
 const getByName = (req, res) => {
   const admin = admins.find(admin => admin.name === req.params.name);
   if (admin) return res.status(200).json(admin);
   return res.status(404).send("Admin not found");
-  }
+}
 
 
 const update = (req, res) => {
   const found = admins.some(admin => admin.id === req.params.id);
   if (found) {
-  const updateAdmin = req.query;
-  admins.forEach(admin => {
-    if (admin.id === req.params.id) {
-      admin.name = updateAdmin.name ? updateAdmin.name : admin.name;
-      admin.email = updateAdmin.email ? updateAdmin.email : admin.email;
-      admin.username = updateAdmin.username ? updateAdmin.username : admin.username;
-      admin.password = updateAdmin.password ? updateAdmin.password : admin.password;
-      return res.status(200).json(admin);
-    }
-  });
-  } 	return res.status(404).send("Admin not found");
-  };
+    const updateAdmin = req.query;
+    admins.forEach(admin => {
+      if (admin.id === req.params.id) {
+        admin.name = updateAdmin.name ? updateAdmin.name : admin.name;
+        admin.email = updateAdmin.email ? updateAdmin.email : admin.email;
+        admin.username = updateAdmin.username ? updateAdmin.username : admin.username;
+        admin.password = updateAdmin.password ? updateAdmin.password : admin.password;
+        return res.status(200).json(admin);
+      }
+    });
+  }
+  return res.status(404).send("Admin not found");
+}
 
 module.exports = {
   getAll,
   getById,
   getByName,
   update
-};
+}
