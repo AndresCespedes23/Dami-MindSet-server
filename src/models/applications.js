82b2703c94d71f14cba37bdd91209135a58fd6fc
Damián Alvarez <alvarezdami93@gmail.com>
2021-11-12 17:30:13 -0300

    MS-46: Add applications model

---
+++ src/models/applications.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ApplicationsSchema = new Schema({
+  id: Schema.Types.ObjectId,
+  idPosition: {
+    type: Schema.Types.ObjectId,
+    ref: "Positions",
+    required: true,
+  },
+  idCandidate: {
+    type: Schema.Types.ObjectId,
+    ref: "Candidates",
+    required: true,
+  },
+  idInterview: {
+    type: Schema.Types.ObjectId,
+    ref: "Interviews",
+    required: true,
+  },
+  dateTime: {
+    type: Date,
+    required: false,
+  },
+  status: {
+    enum: ["PENDING", "SCHEDULED", "HIRED", "REJECTED"],
+    required: true,
+  },
+  result: {
+    type: String,
+    max: 500,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Applications", ApplicationsSchema);
