7e36d580142e424ae0a229dae2d5a6af8c0a7656
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 12:25:38 -0300

    MS-51: Add endpoint to get one profile by Id

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -2,5 +2,6 @@ const profiles = require("../controllers/profiles");
 const router = require("express").Router();
 
 router.get("/", profiles.getAll);
+router.get("/:id", profiles.getById);
 
 module.exports = router;
