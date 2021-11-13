2809731a4c38ff0e45eaf2b2fd7c0344bbe9b676
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 16:32:52 -0300

    MS-51: Add profiles routes to routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const applicationsRoutes = require("./applications");
+const profilesRoutes = require("./profiles");
 const router = express.Router();
 
 router.use("/applications", applicationsRoutes);
+router.use("/profiles", profilesRoutes);
 
 module.exports = router;
