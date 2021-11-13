cd9e26cddda4e21d6e1f664f70154975587a5451
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 19:03:44 -0300

    MS-51: Delete the extra validation

--- src/controllers/profiles.js
+++ src/controllers/profiles.js
@@ -22,9 +22,6 @@ const getById = (req, res) => {
 };
 
 const create = (req, res) => {
-  if (!req.body.name || !req.body.description) {
-    return res.status(400).json({ msg: "Some parameters are missing" });
-  }
   const newProfile = {
     name: req.body.name,
     description: req.body.description,
