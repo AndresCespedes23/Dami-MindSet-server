189c443e74944afeba6a3008cb1e886c3416a1e3
Agustin Macazzaga <ag.macazz@gmail.com>
2021-11-13 15:57:07 -0300

    MS-48 index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const applicationsRoutes = require("./applications");
+const adminsRoutes = require("./admins");
 const router = express.Router();
 
 router.use("/applications", applicationsRoutes);
+router.use("./admins",adminsRoutes);
 
 module.exports = router;
