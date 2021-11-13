d5e77623c8f08271be87a5831505547325a4b40d
nicolasblasco <nicoblasco96@gmail.com>
2021-11-13 14:05:47 -0300

    MS-45: Import positions routes & add positions router endpoint in index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const applicationsRoutes = require("./applications");
+const positionsRoutes = require("./positions");
 const router = express.Router();
 
 router.use("/applications", applicationsRoutes);
+router.use("/positions", positionsRoutes);
 
 module.exports = router;
