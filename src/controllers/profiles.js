533e5571a6e4d784dc87cfd7018363741b2d8dfb
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 13:01:17 -0300

    MS-51: Change require, change getAll & getById functions

--- src/controllers/profiles.js
+++ src/controllers/profiles.js
@@ -1,65 +1,28 @@
-const fs = require("fs");
-const profiles = JSON.parse(fs.readFileSync("./data/profiles.json"));
+const Profiles = require("../models/profiles");
+const ObjectId = require("mongoose").Types.ObjectId;
 
-// Get Profiles
 const getAll = (req, res) => {
-  return res.status(200).json(profiles);
+  Profiles.find()
+    .then((profiles) => {
+      return res.status(200).json(profiles);
+    })
+    .catch((err) => {
+      return res.status(400).json(err);
+    });
 };
 
-// Get Profile by Id
 const getById = (req, res) => {
-  const profile = profile.find((profile) => profile.id === req.params.id);
-  if (profile) return res.status(200).json(profile);
-  return res.status(404).send("Profile not found");
-};
-
-// Create Profile
-const create = (req, res) => {
-  if (!req.query.id || !req.query.name || !req.query.description) {
-    return res.status(400).send("Some parameters are missing");
-  }
-  const newProfile = {
-    id: (profiles.length + 1).toString(),
-    name: req.query.name,
-    description: req.query.description,
-  };
-  return res.status(201).json(newProfile);
-};
-
-// Update Profile
-const update = (req, res) => {
-  const profile = profiles.find((profile) => profile.id === req.params.id);
-  const selectedProfile = profiles.findIndex(
-    (profile) => profile.id === req.params.id
-  );
-  if (profile) {
-    const updateProfile = req.query;
-    profile.name = updateProfile.name || profile.name;
-    profile.description = updateProfile.description || profile.description;
-    profiles[selectedProfile] = profile;
-    return res.status(200).json(selectedProfile);
-  }
-  return res.status(404).send("Error: profile does not exist");
-};
-
-// Remove Profile
-const remove = (req, res) => {
-  const profile = profiles.find((profile) => profile.id === req.params.id);
-  const selectedProfile = profiles.findIndex(
-    (profile) => profile.id === req.params.id
-  );
-  if (profile) {
-    profiles.splice(selectedProfile, 1);
-    return res.status(200).send("Profile removed");
-  }
-  return res.status(404).send("Error: profile does not exist");
+  Profiles.findById({_id: new ObjectId(req.params.id)})
+    .then((profile) => {
+      return res.status(200).json(profile);
+    })
+    .catch((err) => {
+      return res.status(400).json(err);
+    });
 };
 
 // Module Exports
 module.exports = {
   getAll,
   getById,
-  create,
-  update,
-  remove,
 };
