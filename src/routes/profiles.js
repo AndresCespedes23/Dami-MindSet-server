9222e4c847d7b405ae61575a419027e18a98553b
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 12:40:39 -0300

    MS-51: Add endpoint to update a profile

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 
 router.get("/", profiles.getAll);
 router.get("/:id", profiles.getById);
+router.put("/:id", profiles.update);
 
 module.exports = router;
