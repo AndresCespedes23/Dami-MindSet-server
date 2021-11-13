c0dad2735a905a71b8a68602ed92f40997b0a1aa
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 17:28:25 -0300

    MS-51: Delete id from new Schema

--- src/models/profiles.js
+++ src/models/profiles.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProfilesSchema = new Schema({
-  id: {
-    type: Schema.Types.ObjectId,
-    required: true,
-  },
   name: {
     type: String,
     maxLength: 50,
