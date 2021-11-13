1ad203a28a828cff5457225c587e46ea26e7a74c
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 12:42:17 -0300

    MS-51: Add endpoint to create a profile

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 router.get("/", profiles.getAll);
 router.get("/:id", profiles.getById);
 router.put("/:id", profiles.update);
+router.post("/", profiles.create);
 
 module.exports = router;
