ada5f505afb5810c5049cff3ad342bae940a3244
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 11:42:47 -0300

    MS-51: Complete ProfilesSchema

--- src/models/profiles.js
+++ src/models/profiles.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProfilesSchema = new Schema({});
+const ProfilesSchema = new Schema({
+  id: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  name: {
+    type: String,
+    maxLength: 50,
+    required: true
+  },
+  description: {
+    type: String,
+    maxLength: 500,
+    required: true
+  }
+});
 
 module.exports = mongoose.model("Profiles", ProfilesSchema);
