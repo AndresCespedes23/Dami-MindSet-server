947cd087b0e7497ac25fda995814adff50302246
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-13 19:26:04 -0300

    MS-51: Add next(), typeof validation & change if conditions

--- src/validations/profiles.js
+++ src/validations/profiles.js
@@ -1,23 +1,29 @@
-const required = (req, res) => {
+const required = (req, res, next) => {
   if (!req.body.name) {
     return res.status(400).send("Name is required");
   }
   if (!req.description.name) {
     return res.status(400).send("Description is required");
   }
+  next();
 };
 
-const validate = (req, res) => {
+const validate = (req, res, next) => {
   const name = req.body.name;
   const description = req.body.description;
-  if (name.length > 50) {
+  if (name && name.length > 50) {
     return res.status(400).send("Name must have less than 50 characters");
+  } else if (typeof name !== "string") {
+    return res.status(400).send("Name must be string");
   }
-  if (description.length > 500) {
+  if (description && description.length > 500) {
     return res
       .status(400)
       .send("Description must have less than 500 characters");
+  } else if (typeof description !== "string") {
+    return res.status(400).send("Description must be string");
   }
+  next();
 };
 
 module.exports = {
