1ca288036ce86370fe57d4cad16c0104ef419ed3
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-14 18:50:57 -0300

    MS-47: pritier modifications

--- src/models/psychologists.js
+++ src/models/psychologists.js
@@ -5,37 +5,37 @@ const PsychologistsSchema = new Schema({
   name: {
     type: String,
     lowercase: true,
-    maxLength:30,
+    maxLength: 30,
     required: true,
   },
   email: {
     type: String,
-    maxLength:30,
+    maxLength: 30,
     required: true,
   },
   username: {
     type: String,
-    maxLength:30,
+    maxLength: 30,
     required: true,
   },
   password: {
     type: String,
-    maxLength:30,
+    maxLength: 30,
     required: true,
   },
   phoneNumber: {
     type: String,
-    maxLength:15,
+    maxLength: 15,
     required: true,
   },
   enrollmentNumber: {
     type: String,
-    maxLength:4,
+    maxLength: 4,
     required: true,
   },
   status: {
     type: Boolean,
-    required:true,
+    required: true,
   },
   timeRange: {
     type: [Number],
