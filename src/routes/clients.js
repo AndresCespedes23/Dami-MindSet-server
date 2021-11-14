b321f9641f1d8f903daa51f4b50f78157b0043a7
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-14 14:04:49 -0300

    MS-52: Move all endpoints to a separate file on routes folder

---
+++ src/routes/clients.js
@@ -0,0 +1,10 @@
+const controller = require("../controllers/clients");
+const router = require("express").Router();
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.remove);
+
+module.exports = router;
