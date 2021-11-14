696f8524763b6e429be3ec96f6a77c87d2580f05
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-14 14:19:56 -0300

    MS-52: Import clients routes on routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const applicationsRoutes = require("./applications");
+const clientsRoutes = require("./clients");
 const router = express.Router();
 
 router.use("/applications", applicationsRoutes);
+router.use("/clients", clientsRoutes);
 
 module.exports = router;
