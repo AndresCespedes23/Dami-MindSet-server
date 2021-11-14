b1b0c78c68b89a5f6afe80697abb78cf7851ee34
nicolasblasco <nicoblasco96@gmail.com>
2021-11-14 20:49:54 -0300

    MS-45: Fix update function by adding idProfileArray & forEach function

--- src/controllers/positions.js
+++ src/controllers/positions.js
@@ -46,9 +46,13 @@ const create = (req, res) => {
 };
 
 const update = (req, res) => {
+  const idProfileArray = [];
+  req.body.idProfile.forEach((id) => {
+    idProfileArray.push(new ObjectId(id));
+  });
   const updatedPosition = {
     idClient: new ObjectId(req.body.idClient),
-    idProfile: new ObjectId(req.body.idProfile),
+    idProfile: idProfileArray,
     name: req.body.name,
     description: req.body.description,
     status: req.body.status,
