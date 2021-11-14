150c56e802a954bff46bc1e8e3d389328a108e56
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-14 14:00:01 -0300

    MS-52: Create mongoose model

---
+++ src/models/clients.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ClientSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  cuit: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  activity: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Client", ClientSchema);
