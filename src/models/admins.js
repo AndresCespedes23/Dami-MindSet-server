8af30c1879ebe92fe54ea8aa00e7435b1882a27e
Agustin Macazzaga <ag.macazz@gmail.com>
2021-11-15 19:45:40 -0300

    MS-48 type: unique add on adm schema

--- src/models/admins.js
+++ src/models/admins.js
@@ -5,21 +5,26 @@ const AdminsSchema = new Schema({
     type: String,
     maxLength: 50,
     required: true,
+
   },
   email: {
     type: String,
     maxLength: 20,
     required: true,
+    unique: true
+
   },
   username: {
     type: String,
     maxLength: 50,
     required: true,
+    unique: true
   },
   password: {
     type: String,
     maxLength: 20,
     required: true,
+
   },
   isSuperAdmin: {
     type: Boolean,
