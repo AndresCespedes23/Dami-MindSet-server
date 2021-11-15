4b54e2f12dd1f1242ebcfcc194b8bbb064944237
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-15 10:15:01 -0300

    MS-51: Fix conflict in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const applicationsRoutes = require("./applications");
 const profilesRoutes = require("./profiles");
+const psychologistsRoutes = require("./psychologists");
 const router = express.Router();
 
 router.use("/applications", applicationsRoutes);
 router.use("/profiles", profilesRoutes);
+router.use("/psychologists", psychologistsRoutes);
 
 module.exports = router;
