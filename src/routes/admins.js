59cf07302cf86d1e63e00b325a2daae44683ef5d
Agustin Macazzaga <ag.macazz@gmail.com>
2021-11-15 15:45:49 -0300

    MS-48 validation on admins update

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,10 +1,10 @@
 const admins = require("../controllers/admins");
-//const validations = require("../validations/admins");
+const validations = require("../validations/admins");
 const router = require("express").Router();
 
 router.get("/", admins.getAll);
 router.get("/:id", admins.getById);
 router.get("/byName/:name", admins.getByName);
-router.put("/:id", admins.update); //MISSING VALIDATION ON UPDATE
+router.put("/:id",validations.required,admins.update);
 
 module.exports = router;
