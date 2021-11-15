fb7942432d9b9efcf31041ab84dd716eacaa98ee
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-15 14:10:27 -0300

    MS-52: Add regexp match on mongoose schema to validate format

--- src/models/clients.js
+++ src/models/clients.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -7,36 +8,42 @@ const ClientsSchema = new Schema({
     required: true,
     minLength: 6,
     maxLength: 50,
+    match: /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/,
   },
   email: {
     type: String,
     required: true,
     minLength: 6,
     maxLength: 50,
+    match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   },
   phoneNumber: {
     type: String,
     required: true,
     minLength: 6,
     maxLength: 15,
+    match: /^\d+$/,
   },
   cuit: {
     type: String,
     required: true,
     minLength: 6,
     maxLength: 11,
+    match: /^\d+$/,
   },
   address: {
     type: String,
     required: true,
     minLength: 6,
     maxLength: 50,
+    match: /^[a-zA-Z0-9\s,'-]*$/,
   },
   activity: {
     type: String,
     required: true,
     minLength: 3,
     maxLength: 20,
+    match: /^([^0-9]*)$/,
   },
 });
 
