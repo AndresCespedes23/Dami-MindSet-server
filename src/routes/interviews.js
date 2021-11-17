7f979c528f3b74ead3f9efaeceb233c4f6d941a8
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-16 21:42:30 -0300

    MS-50: add require validations to routes folder

--- src/routes/interviews.js
+++ src/routes/interviews.js
@@ -2,8 +2,8 @@ const interviews = require("../controllers/interviews");
 const validations = require("../validations/interviews");
 const router = require("express").Router();
 
-router.post("/", validations.validate, interviews.create);
-router.put("/:id", validations.validate, interviews.update);
+router.post("/", validations.require, validations.validate, interviews.create);
+router.put("/:id",validations.require, validations.validate, interviews.update);
 router.delete("/:id", interviews.remove);
 router.get("/", interviews.getAll);
 router.get("/:id", interviews.getById);
