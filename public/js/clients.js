a25694527584c6c258e93c7a4875d0867eb8aa08
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-17 19:44:15 -0300

    MS-57: draft clients views

--- public/js/clients.js
+++ public/js/clients.js
@@ -1,24 +1,28 @@
-function sendServer() {
-  var url = "https://localhost:4000/";
+window.onload = function () {
+// function sendServer() {
+  var url = "http://localhost:4000/api/clients/61929b6bb95f17d609395ad1";
 
   fetch(url)
-    .then(function respuesta(res) {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error(`HTTP ${res.status}`);
-      }
+    .then((res) => {
+      if (res.status === 200) return res.json();
+      throw new Error(`HTTP ${res.status}`);
     })
-    // .then(function info(data){
-    //     for (const property in data){
-    //         ulModal.innerHTML+=`<li>${property}: ${data[property]}</li>`;
-    //         localStorage.setItem(property, data[property]);  // save the info in localStorage
-    //     }
-    // })
-    .catch(function faiulure(error) {
+    .then((data) => {
+      createList(data);
+    })
+    .catch((error) => {
       console.log(error);
-      sendToServer.classList.toggle("hide", true); //hide the buttom send
-      titleModal.innerText = "SUBSCRIPTION FAILED";
-      ulModal.innerHTML = `<li>${error}</li>`;
     });
+
+
+    function createList(client){
+      const table =  document.getElementById("table-list")
+      let itemList = document.createElement('tr');
+      itemList.innerHTML = `<td>${client._id}</td><td>${client.name}</td><td>${client.email}</td><td>${client.phoneNumber}</td><td>${client.cuit}</td><td>${client.address}</td><td>${client.activity}</td><td><img src="img/Icon-edit.png" alt="Edit" /></td>
+      <td><a href="http://localhost:4000/api/clients/${client._id}"><img src="img/Icon-remove.png" alt="Remove"/></a></td>`
+      table.appendChild(itemList);
+
+    }
+
 }
+
