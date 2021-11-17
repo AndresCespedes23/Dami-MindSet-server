9e4e9fd58ede0b2f98f2fe86ee26aa675d60894f
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-16 21:43:21 -0300

    MS-50: pritier

--- src/routes/interviews.js
+++ src/routes/interviews.js
@@ -3,7 +3,12 @@ const validations = require("../validations/interviews");
 const router = require("express").Router();
 
 router.post("/", validations.require, validations.validate, interviews.create);
-router.put("/:id",validations.require, validations.validate, interviews.update);
+router.put(
+  "/:id",
+  validations.require,
+  validations.validate,
+  interviews.update
+);
 router.delete("/:id", interviews.remove);
 router.get("/", interviews.getAll);
 router.get("/:id", interviews.getById);
