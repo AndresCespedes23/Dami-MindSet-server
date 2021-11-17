5dc43bd4516c1c97e3f0ef0494f8ba2dfb071c97
Esteban Frare <esteban.frare@radiumrocket.com>
2021-11-17 15:25:47 -0300

    MS: setup heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 4000;
 const router = require("./routes");
 
 // // Routes
