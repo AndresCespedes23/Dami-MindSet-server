ed49d2a897ff2ee92c34ee06ea31a5b50d0c1bad
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-18 10:08:49 -0300

    MS-57: commit to update with develop

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,5 +1,5 @@
-const controller = require("../controllers/clients");
 const router = require("express").Router();
+const controller = require("../controllers/clients");
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
