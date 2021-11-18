eb478ea8cd776951414ed66c7ab3e2219a344809
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-18 20:48:39 -0300

    MS-57: create validations clients in the backend, add middlewares in clients routes,  add draft  delete modal and its interactive functions

--- public/js/clients.js
+++ public/js/clients.js
@@ -1,12 +1,17 @@
 const createModal = document.getElementById("create-modal");
 const updateModal = document.getElementById("update-modal");
+const deleteModal = document.getElementById("delete-modal");
 const createButton = document.getElementById("create-button");
 const submitCreateButton = document.getElementById("confirm-create-button");
 const submitUpdateButton = document.getElementById("confirm-update-button");
+const submitDeleteButton = document.getElementById("confirm-delete-button");
+const cancelDeleteButtom = document.getElementById("cancel-delete-button");
 
 createButton.addEventListener("click", openCreateModal);
 submitCreateButton.addEventListener("click", reqCreateClient);
 submitUpdateButton.addEventListener("click", reqUpdateClient);
+submitDeleteButton.addEventListener("click", reqDeleteClient);
+cancelDeleteButtom.addEventListener("click", closeDeleteModal);
 
 createButton.disabled = true; //disable the button until the page is completely loaded.
 readClients();
@@ -47,7 +52,7 @@ function reqCreateClient(e) {
   })
     .then((res) => {
       if (res.status === 201) return res.json();
-      throw new Error(`HTTP ${res.status}`);
+      throw new Error(JSON.stringify(res.json()));
     })
     .then((data) => {
       createModal.classList.toggle("hide", true);
@@ -87,8 +92,7 @@ function createList(clients) {
       <td>${client.address}</td>
       <td>${client.activity}</td>
       <td><button class="button-list" onclick="openUpdateModal(${i})"><img src="img/Icon-edit.png" alt="Edit"></button></td>
-      <td><button class="button-list" onclick="deleteClient(${i})"><img src="img/Icon-remove.png" alt="Remove"/></button>
-      </form></td>`;
+      <td><button class="button-list" onclick="openDeleteModal(${i})"><img src="img/Icon-remove.png" alt="Remove"/></button>`;
     table.appendChild(itemList);
     i++;
   });
@@ -98,7 +102,6 @@ function createList(clients) {
 // UPDATE CLIENTS********************************************************
 function openUpdateModal(index) {
   updateModal.classList.toggle("hide", false);
-
   const dataForm = document.querySelectorAll("#update-form input");
   let contentItem = document.getElementById(`item-${index}`).firstChild;
   for (let i = 0; i < dataForm.length; i++) {
@@ -150,9 +153,16 @@ function reqUpdateClient(e) {
 }
 
 // DELETE CLIENTS***************************************************
-function deleteClient(index) {
-  const itemToDelete = document.getElementById(`item-${index}`);
-  const idClient = itemToDelete.firstElementChild.innerText;
+function openDeleteModal(index) {
+  deleteModal.classList.toggle("hide", false);
+
+  const idClient = document.getElementById("idClientDelete");
+  let idClientElement = document.getElementById(`item-${index}`).firstChild;
+  idClient.innerText = idClientElement.innerText;
+}
+
+function reqDeleteClient(){
+  const idClient = document.getElementById("idClientDelete").innerText;
 
   const url = "http://localhost:4000/api/clients/" + idClient;
   fetch(url, {
@@ -168,4 +178,8 @@ function deleteClient(index) {
     .catch((error) => {
       console.log(error);
     });
+}
+
+function closeDeleteModal(index) {
+  deleteModal.classList.toggle("hide", true);
 }
--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 const controller = require("../controllers/clients");
 
+const middleware = require("../validations/clients");
+
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
-router.post("/", controller.create);
-router.put("/:id", controller.update);
+router.post("/", middleware.dataBodyRequired, controller.create);
+router.put(
+  "/:id",
+  // middleware.formatBodyRequired,
+  controller.update,
+);
 router.delete("/:id", controller.remove);
 
 module.exports = router;
