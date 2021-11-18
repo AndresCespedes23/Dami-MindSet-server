35933f50f2b51dbf85995ee81f5a354e55a721f9
nicolasblasco <nicoblasco96@gmail.com>
2021-11-18 18:46:22 -0300

    MS-58: Add function to request all interviews

--- public/js/interviews.js
+++ public/js/interviews.js
@@ -1,1 +1,17 @@
-window.onload = function() {}
+window.onload = function() {
+
+  function requestInterviews() {
+    const url = "http://localhost:4000/api/interviews/";
+    fetch(url)
+      .then((res) => {
+        if (res.status === 200) return res.json();
+        throw new Error(`HTTP ${res.status}`);
+      })
+      .then((data) => {
+        createList(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
