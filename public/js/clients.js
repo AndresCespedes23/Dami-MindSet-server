f0a3396d389369e31fcfaacdf2a6e041fd684e92
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-11-17 21:01:23 -0300

    MS-57: create client.js in public. add view list of clients. buttoms doesnt work yet

--- public/js/clients.js
+++ public/js/clients.js
@@ -1,7 +1,5 @@
 window.onload = function () {
-  // function sendServer() {
-  var url = "http://localhost:4000/api/clients/61929b6bb95f17d609395ad1";
-
+  var url = "http://localhost:4000/api/clients/";
   fetch(url)
     .then((res) => {
       if (res.status === 200) return res.json();
@@ -13,12 +11,40 @@ window.onload = function () {
     .catch((error) => {
       console.log(error);
     });
+}
 
-  function createList(client) {
-    const table = document.getElementById("table-list");
+function createList(clients) {
+  const table = document.getElementById("table-list");
+  clients.forEach(client => {
     let itemList = document.createElement("tr");
-    itemList.innerHTML = `<td>${client._id}</td><td>${client.name}</td><td>${client.email}</td><td>${client.phoneNumber}</td><td>${client.cuit}</td><td>${client.address}</td><td>${client.activity}</td><td><img src="img/Icon-edit.png" alt="Edit" /></td>
-      <td><a href="http://localhost:4000/api/clients/${client._id}"><img src="img/Icon-remove.png" alt="Remove"/></a></td>`;
+    itemList.innerHTML = `<td>${client._id}</td>
+      <td>${client.name}</td>
+      <td>${client.email}</td>
+      <td>${client.phoneNumber}</td>
+      <td>${client.cuit}</td>
+      <td>${client.address}</td>
+      <td>${client.activity}</td>
+      <td><button class="button-list" onclick="updateClient(${client._id})"><img src="img/Icon-edit.png" alt="Edit"></button></td>
+      <td><button class="button-list" onclick="deleteClient(${client._id})"><img src="img/Icon-remove.png" alt="Remove"/></button>
+      </form></td>`
     table.appendChild(itemList);
-  }
+  });
 };
+
+function deleteClient(idClient){
+  console.log('entre')
+  const opc = {method: "DELETE"};
+
+  var request = new Request (`http://localhost:4000/api/clients/${idClient}`, opc );
+  fetch(request)
+    .then((res) => {
+      if (res.status === 200) return res.json();
+      throw new Error(`HTTP ${res.status}`);
+    })
+    .then((data) => {
+      createList(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
