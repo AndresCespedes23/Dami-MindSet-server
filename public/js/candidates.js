a5b53ccea6d86794a2235d77bc3f1330cdaba29e
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-19 14:42:25 -0300

    MS-66: Implement remove candidate function

--- public/js/candidates.js
+++ public/js/candidates.js
@@ -9,6 +9,63 @@ const getAge = (dateString) => {
   return age;
 }
 
+function wipeTable() {
+  const table = document.getElementById("resource-table");
+  table.innerHTML = `<tr id="table-headers">
+    <th>id</th>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Address</th>
+    <th>Status</th>
+    <th>Profiles</th>
+    <th>See applications</th>
+    <th>See Profile</th>
+    <th></th>
+    <th></th>
+    </tr>`
+}
+
+function showProfile() {
+  //function content
+}
+
+function showApplications() {
+  //function content
+}
+
+function editCandidate() {
+  //function content
+}
+
+function showConfirmModal() {
+  candidate = this;
+  modal.classList.toggle("show");
+  modalContent.classList.toggle("show");
+  modalContent.querySelector("#yes").onclick = function(){
+    modal.classList.toggle("show");
+    modalContent.classList.toggle("show");
+    wipeTable();
+    removeCandidate(candidate);
+  };
+}
+
+function removeCandidate(candidate) {
+  let url = `http://localhost:4000/api/candidates/${candidate._id}`;
+  fetch(url, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      if (res.status === 200) return res.json();
+      throw new Error(`HTTP ${res.status}`);
+    })
+    .then(() => {
+      readCandidates();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 function readCandidates() {
   let url = "http://localhost:4000/api/candidates";
   fetch(url)
@@ -26,7 +83,6 @@ function readCandidates() {
 
 function createList(candidates) {
   const table = document.getElementById("resource-table");
-  console.log(table);
   candidates.forEach((candidate) => {
     const row = document.createElement("tr");
     row.innerHTML = `
@@ -36,13 +92,23 @@ function createList(candidates) {
     <td>${candidate.address}</td>
     <td>${candidate.status}</td>
     <td>${candidate.profiles.join(",")}</td>
-    <td><img src="img/Icon-visible.png" alt="Show" /></td>
-    <td><img src="img/Icon-visible.png" alt="Show" /></td>
-    <td><img src="img/Icon-visible.png" alt="Show" /></td>
-    <td><img src="img/Icon-edit.png" alt="Edit" /></td>
-    <td><img src="img/Icon-remove.png" alt="Remove" /></td>`;
+    <td><img src="img/Icon-visible.png" alt="Show" id="show-applications" /></td>
+    <td><img src="img/Icon-visible.png" alt="Show" id="show-profile" /></td>
+    <td><img src="img/Icon-edit.png" alt="Edit" id="edit" /></td>
+    <td><img src="img/Icon-remove.png" alt="Remove" id="remove" /></td>`;
     table.appendChild(row);
+    row.querySelector("#show-profile").addEventListener("click", showProfile.bind(candidate));
+    row.querySelector("#show-applications").addEventListener("click", showApplications.bind(candidate));
+    row.querySelector("#edit").addEventListener("click", editCandidate.bind(candidate));
+    row.querySelector("#remove").addEventListener("click", showConfirmModal.bind(candidate));
   })
 }
 
 readCandidates();
+
+const modal = document.getElementById("background-modal");
+const modalContent = document.getElementById("confirm-modal");
+modalContent.querySelector("#no").addEventListener("click", () => {
+  modal.classList.toggle("show")
+  modalContent.classList.toggle("show");
+});
