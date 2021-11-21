9989bfe798605365190320da48f88ad2e41c13a0
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-21 14:41:14 -0300

    MS-54: change the way to close the modal by add or remove a class

--- public/js/admins.js
+++ public/js/admins.js
@@ -9,6 +9,7 @@ const adminModal = document.getElementById("background-modal");
 
 //the modal's form
 const formModal = document.querySelector("form");
+const closeModal = document.getElementById("closeModal")
 
 //the modal's elements
 const adminName = document.getElementById("adminName");
@@ -29,8 +30,8 @@ const showData = (admins) => {
         <td>${admin.password}</td>
         <td>${admin.isSuperAdmin}</td>
         <td><button class="editButton"><img src="img/Icon-edit.png" alt="EditAdmin"/></button></td>
-        <td><button class="deleteButton"><img src="img/Icon-remove.png" alt="Remove"/></button></td>
-      </tr>`;
+      </tr>
+    `;
   })
   container.innerHTML = results;
 }
@@ -51,3 +52,69 @@ const showData = (admins) => {
     .catch((err) => {
       console.log(err);
     });
+
+
+//Function to UPDATE an admin
+const on = (element, event, selector, handler) => {
+  element.addEventListener(event, e => {
+    if (e.target.closest(selector)) {
+      return handler(e);
+    }
+  })
+};
+on(document, 'click', '.editButton', e => {
+  const adminsLane = e.target.parentNode.parentNode.parentNode
+  const adminsName = adminsLane.children[0].innerHTML
+  const adminsEmail = adminsLane.children[1].innerHTML
+  const adminsUsername = adminsLane.children[2].innerHTML
+  const adminsPassword = adminsLane.children[3].innerHTML
+  const adminsIsSuperAdmin = adminsLane.children[4].innerHTML
+  adminName.value = adminsName
+  EMail.value = adminsEmail
+  userName.value = adminsUsername
+  passWord.value = adminsPassword
+  superAdmin.value = adminsIsSuperAdmin
+  save = "Edit"
+  adminModal.classList.remove("hide");
+  console.log(adminsName)
+} )
+//Sumbmit changes
+formModal.addEventListener("submit", (e) => {
+  e.preventDefault(e)
+    fetch(url+adminName, {
+      method: "PUT",
+      headers: {
+          "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        name:adminName.value,
+        email:EMail.value,
+        username:userName.value,
+        password:passWord.value,
+        isSuperAdmin:superAdmin.value,
+      })
+    })
+    .then((res) => {
+        if (res.status === 200)
+          return res.json();
+      })
+      .then(Response => location.reload())
+      .catch((err) => {
+        console.log(err);
+      });
+      adminModal.classList.add("hide");
+})
+
+//Cancel changes
+closeModal.addEventListener("click", closeFModal)
+function closeFModal (e){
+    e.preventDefault();
+    adminModal.classList.add("hide");
+}
+//click outside the modal also close it
+window.onclick = function(outside){
+    if (outside.target == adminModal){
+        adminModal.classList.add("hide");
+    }
+}
+adminModal.classList.add("hide")
