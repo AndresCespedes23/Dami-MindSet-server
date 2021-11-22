d2bffdee430456f77e9d587eadd6db4f411bdb08
David Mansilla <davidjamirmansilla@gmail.com>
2021-11-21 21:18:31 -0300

    MS-54: ready to revision

--- public/js/admins.js
+++ public/js/admins.js
@@ -1,43 +1,43 @@
-const url = "http://localhost:4000/api/admins/";
+  //URl
+  const url = "http://localhost:4000/api/admins/";
 
-//catch the table's body and results
-const container = document.querySelector("tbody");
-let results = "";
+  //catch the table's body and results
+  const container = document.querySelector("tbody");
+  let results = "";
 
-//catching the modal
-const adminModal = document.getElementById("background-modal");
+  //catching the modal
+  const adminModal = document.getElementById("background-modal");
 
-//the modal's form
-const formModal = document.querySelector("form");
-const closeModal = document.getElementById("closeModal")
+  //the modal's form
+  const formModal = document.querySelector("form");
+  const closeModal = document.getElementById("closeModal")
 
-//the modal's elements
-const adminName = document.getElementById("adminName");
-const EMail = document.getElementById("e-mail");
-const userName = document.getElementById("username");
-const passWord = document.getElementById("password");
-const superAdmin = document.getElementById("superadmin");
+  //the modal's elements
+  const adminName = document.getElementById("adminName");
+  const EMail = document.getElementById("e-mail");
+  const userName = document.getElementById("username");
+  const passWord = document.getElementById("password");
+  const superAdmin = document.getElementById("superadmin");
 
-//function to show all the admins in the db
-const showData = (admins) => {
-  admins.forEach(admin => {
-    results +=`
-      <tr>
-        <td>${admin._id}</td>
-        <td>${admin.name}</td>
-        <td>${admin.email}</td>
-        <td>${admin.username}</td>
-        <td>${admin.password}</td>
-        <td>${admin.isSuperAdmin}</td>
-        <td><button class="editButton"><img src="img/Icon-edit.png" alt="EditAdmin"/></button></td>
-      </tr>
-    `;
-  })
-  container.innerHTML = results;
-}
+  //function to show all the admins in the db
+  const showData = (admins) => {
+    admins.forEach(admin => {
+      results +=`
+        <tr>
+          <td>${admin._id}</td>
+          <td>${admin.name}</td>
+          <td>${admin.email}</td>
+          <td>${admin.username}</td>
+          <td>${admin.password}</td>
+          <td>${admin.isSuperAdmin}</td>
+          <td><button class="editButton"><img src="img/Icon-edit.png" alt="EditAdmin"/></button></td>
+        </tr>
+      `;
+    })
+    container.innerHTML = results;
+  }
 
-//show Admins
-
+  //show all the Admins in DataBase
   fetch(url)
     .then((res) => {
       if (res.status === 200)
@@ -51,75 +51,74 @@ const showData = (admins) => {
       console.log(err);
     });
 
-
-//Function to UPDATE an admin
-const on = (element, event, selector, handler) => {
-  element.addEventListener(event, e => {
-    if (e.target.closest(selector)) {
-      return handler(e);
-    }
-  })
-};
-on(document, 'click', '.editButton', e => {
-  const adminsLane = e.target.parentNode.parentNode.parentNode
-  const adminsName = adminsLane.children[1].innerHTML
-  const adminsEmail = adminsLane.children[2].innerHTML
-  const adminsUsername = adminsLane.children[3].innerHTML
-  const adminsPassword = adminsLane.children[4].innerHTML
-  const adminsIsSuperAdmin = adminsLane.children[5].innerHTML
-  adminName.value = adminsName
-  EMail.value = adminsEmail
-  userName.value = adminsUsername
-  passWord.value = adminsPassword
-  superAdmin.value = adminsIsSuperAdmin
-  adminModal.classList.remove("hide");
-} )
-
-//get Ids of the admins
-let adminId = on(document, 'click', '.editButton', e => {
+  //Function to UPDATE an admin
+  const on = (element, event, selector, handler) => {
+    element.addEventListener(event, e => {
+      if (e.target.closest(selector)) {
+        return handler(e);
+      }
+    })
+  };
+  on(document, 'click', '.editButton', e => {
     const adminsLane = e.target.parentNode.parentNode.parentNode
-    adminId = adminsLane.children[0].innerHTML
-});
+    const adminsName = adminsLane.children[1].innerHTML
+    const adminsEmail = adminsLane.children[2].innerHTML
+    const adminsUsername = adminsLane.children[3].innerHTML
+    const adminsPassword = adminsLane.children[4].innerHTML
+    const adminsIsSuperAdmin = adminsLane.children[5].innerHTML
+    adminName.value = adminsName
+    EMail.value = adminsEmail
+    userName.value = adminsUsername
+    passWord.value = adminsPassword
+    superAdmin.value = adminsIsSuperAdmin
+    adminModal.classList.remove("hide");
+  });
 
-//Submit changes
-const submit = (e) => {
-  e.preventDefault(e)
-    fetch(url+adminId, {
-      method: "PUT",
-      headers: {
-          "Content-Type":"application/json"
-      },
-      body: JSON.stringify({
-        name:adminName.value,
-        email:EMail.value,
-        username:userName.value,
-        password:passWord.value,
-        isSuperAdmin:superAdmin.value,
-      })
+  //get Ids of the admins
+  let adminId = on(document, 'click', '.editButton', e => {
+      const adminsLane = e.target.parentNode.parentNode.parentNode
+      adminId = adminsLane.children[0].innerHTML
+    });
+
+  //Submit changes
+  const submit = (e) => {
+    e.preventDefault(e)
+      fetch(url+adminId, {
+        method: "PUT",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          name:adminName.value,
+          email:EMail.value,
+          username:userName.value,
+          password:passWord.value,
+          isSuperAdmin:superAdmin.value,
+        })
     })
     .then((res) => {
-        if (res.status === 200)
-          return res.json();
+      if (res.status === 200)
+      return res.json();
     })
     .then( res => location.reload())
     .catch((err) => {
-        console.log(err);
-      });
+      console.log(err);
+    });
       adminModal.classList.add("hide");
-};
-formModal.addEventListener("submit", submit);
+  };
+  formModal.addEventListener("submit", submit);
 
-//Cancel changes
-closeModal.addEventListener("click", closeFModal)
-function closeFModal (e){
-    e.preventDefault();
-    adminModal.classList.add("hide");
-};
-
-//click outside the modal also close it
-window.onclick = function(outside){
-    if (outside.target == adminModal){
-        adminModal.classList.add("hide");
+  //Cancel changes
+  closeModal.addEventListener("click", closeFModal)
+    function closeFModal (e){
+      e.preventDefault();
+      adminModal.classList.add("hide");
     };
-};
-adminModal.classList.add("hide");
+
+  //click outside the modal also close it
+  window.onclick = function(outside) {
+    if (outside.target == adminModal) {
+      adminModal.classList.add("hide");
+    };
+    };
+  adminModal.classList.add("hide");
--- public/js/validations4Admins.js
+++ public/js/validations4Admins.js
@@ -1,95 +1,95 @@
-// get all the elements by the ID
-const nameAdmin = document.getElementById("adminName");
-const eMail = document.getElementById("e-mail");
-const adminsUserName = document.getElementById("username");
-const adminPassWord = document.getElementById("password");
-const confirmB = document.getElementById("confirm-button");
-confirmB.classList.remove("hide");
-//get all the errors
-const nameError = document.getElementById("Error1");
-const eMailError = document.getElementById("Error2");
-const adminsNameError = document.getElementById("Error3");
-const adminPassWordError = document.getElementById("Error4");
-const isSuperAdminError = document.getElementById("Error5");
-nameError.innerText = "";
-eMailError.innerText = "";
-adminsNameError.innerText = "";
-adminPassWordError.innerText = "";
-
-//validations for the admin's name
-nameAdmin.addEventListener("focus", namefocus);
-function namefocus(e) {
-  e.preventDefault();
+  // get all the elements by the ID
+  const nameAdmin = document.getElementById("adminName");
+  const eMail = document.getElementById("e-mail");
+  const adminsUserName = document.getElementById("username");
+  const adminPassWord = document.getElementById("password");
+  const confirmB = document.getElementById("confirm-button");
+  confirmB.classList.remove("hide");
+  //get all the errors
+  const nameError = document.getElementById("Error1");
+  const eMailError = document.getElementById("Error2");
+  const adminsNameError = document.getElementById("Error3");
+  const adminPassWordError = document.getElementById("Error4");
+  const isSuperAdminError = document.getElementById("Error5");
   nameError.innerText = "";
-};
+  eMailError.innerText = "";
+  adminsNameError.innerText = "";
+  adminPassWordError.innerText = "";
 
-nameAdmin.addEventListener("blur", adminBlur);
-function adminBlur(e) {
-  e.preventDefault();
-  const onlyTxt = (/[a-zA-Z]$/);
-  let n = nameAdmin.value;
-    if (n.length < 6 || n.indexOf(" ") <= 0 || n.indexOf(" ") == n.length -1) {
+  //validations for the admin's name
+  nameAdmin.addEventListener("focus", namefocus);
+    function namefocus(e) {
+      e.preventDefault();
+      nameError.innerText = "";
+    };
+
+  nameAdmin.addEventListener("blur", adminBlur);
+    function adminBlur(e) {
+      e.preventDefault();
+      const onlyTxt = (/[a-zA-Z]$/);
+      let n = nameAdmin.value;
+      if (n.length < 6 || n.indexOf(" ") <= 0 || n.indexOf(" ") == n.length -1) {
       confirmB.classList.add("hide");
       return nameError.innerText = "The name must be longer than 6 characters & have at least one space between name and surname";
-    } if (!onlyTxt.test(n)) {
+      } if (!onlyTxt.test(n)) {
       confirmB.classList.add("hide");
       return nameError.innerText = "Only text allowed";
-    } confirmB.classList.remove("hide");
+      } confirmB.classList.remove("hide");
       return nameError.innerText = "✓";
+    };
 
-};
+  //validations for admin's e-mail
+  eMail.addEventListener("focus", mailFocus);
+    function mailFocus(e) {
+      e.preventDefault();
+      eMailError.innerText = "";
+    };
 
-//validations for admin's e-mail
-eMail.addEventListener("focus", mailFocus);
-function mailFocus(e) {
-  e.preventDefault();
-  eMailError.innerText = "";
-};
+  eMail.addEventListener("blur", mailBlur);
+    function mailBlur(e) {
+      e.preventDefault();
+      let eV = eMail.value;
+      let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+      if (!emailFormat.test(eV)) {
+      confirmB.classList.add("hide");
+      return eMailError.innerText = "Invalid E-Mail Format";
+      } confirmB.classList.remove("hide");
+      return eMailError.innerText = "✓";
+    };
 
-eMail.addEventListener("blur", mailBlur);
-function mailBlur(e) {
-  e.preventDefault();
-  let eV = eMail.value;
-  let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-  if (!emailFormat.test(eV)) {
-    confirmB.classList.add("hide");
-    return eMailError.innerText = "Invalid E-Mail Format";
-  } confirmB.classList.remove("hide");
-    return eMailError.innerText = "✓";
-};
+  //Validation for admin's username
+  adminsUserName.addEventListener("focus", userFocus);
+    function userFocus(e) {
+      e.preventDefault();
+      adminsNameError.innerText = "";
+    };
 
-//Validation for admin's username
-adminsUserName.addEventListener("focus", userFocus);
-function userFocus(e) {
-  e.preventDefault();
-  adminsNameError.innerText = "";
-};
+  adminsUserName.addEventListener("blur", userBlur);
+    function userBlur(e) {
+      e.preventDefault();
+      if (adminsUserName.value.length < 2 || adminsUserName.value.length > 20) {
+      confirmB.classList.add("hide");
+      return adminsNameError.innerText = "The UserName must be longer than 2 characters but less than 20";
+      } confirmB.classList.remove("hide");
+      return adminsNameError.innerText = "✓";
+    };
 
-adminsUserName.addEventListener("blur", userBlur);
-function userBlur(e) {
-  e.preventDefault();
-  if (adminsUserName.value.length < 2 || adminsUserName.value.length > 20) {
-    confirmB.classList.add("hide");
-    return adminsNameError.innerText = "The UserName must be longer than 2 characters but less than 20";
-  } confirmB.classList.remove("hide");
-    return adminsNameError.innerText = "✓";
-};
+  // Validations for admin's password
+  adminPassWord.addEventListener("focus", passWFocus);
+    function passWFocus(e) {
+      e.preventDefault();
+      adminPassWordError.innerText = "";
+    };
 
-// Validations for admin's password
-adminPassWord.addEventListener("focus", passWFocus);
-function passWFocus(e) {
-  e.preventDefault();
-  adminPassWordError.innerText = "";
-};
+  adminPassWord.addEventListener("blur", passWBLur);
+    function passWBLur(e) {
+      e.preventDefault();
+      let passValue = adminPassWord.value;
+      let passWFormat = /^[a-z0-9]{8,18}$/;
+      if (!passWFormat.test(passValue)) {
+      confirmB.classList.add("hide");
+      return adminPassWordError.innerText = "The password only accepts letters or numbers and also must have a lenght of 8 characters";
+      } confirmB.classList.remove("hide");
+      return adminPassWordError.innerText = "✓";
+    };
 
-adminPassWord.addEventListener("blur", passWBLur);
-function passWBLur(e) {
-  e.preventDefault();
-  let passValue = adminPassWord.value;
-  let passWFormat = /^[a-z0-9]{8,18}$/;
-  if (!passWFormat.test(passValue)) {
-    confirmB.classList.add("hide");
-    return adminPassWordError.innerText = "The password only accepts letters or numbers and also must have a lenght of 8 characters";
-  } confirmB.classList.remove("hide");
-    return adminPassWordError.innerText = "✓";
-};
