65aff672eb961156e1e319ecea6627feff047e57
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-22 12:15:41 -0300

    MS-66: Write create/edit form

--- public/js/candidates.js
+++ public/js/candidates.js
@@ -33,17 +33,68 @@ function showApplications() {
   //function content
 }
 
-function editCandidate() {
-  //function content
+function openEditForm(candidate) {
+  console.log(candidate.education)
+  const form = createEditModal.querySelector("form");
+  modal.classList.toggle("show");
+  createEditModal.classList.toggle("show");
+  createEditModal.querySelector("#confirm-button").onclick = function(){
+    const data = {
+      personalInfo: {
+        name: form.querySelector("#name").value,
+        email: form.querySelector("#email").value,
+        username: form.querySelector("#username").value,
+        password: form.querySelector("#password").value,
+        gender: form.querySelector("#gender").value,
+        address: form.querySelector("#address").value,
+        phoneNumber: form.querySelector("#phoneNumber").value,
+        dateOfBirth: form.querySelector("#dateOfBirth").value,
+        zipCode: form.querySelector("#zipCode").value,
+        city: form.querySelector("#city").value,
+        state: form.querySelector("#state").value,
+        country: form.querySelector("#country").value,
+        dni: form.querySelector("#dni").value,
+        status: form.querySelector("#status").value
+      },
+      education: [
+        {
+          level: form.querySelector("#level").value,
+          institution: form.querySelector("#institution").value,
+          title: form.querySelector("#title").value,
+          startDate: form.querySelector("#startDate").value,
+          finishDate: form.querySelector("#finishDate").value,
+          inProgress: form.querySelector("#inProgress").value,
+          _id: candidate.education._id
+        }
+      ],
+      workExperience: [
+        {
+          company: form.querySelector("#company").value,
+          role: form.querySelector("#role").value,
+          startDate: form.querySelector("#startDate").value,
+          finishDate: form.querySelector("#finishDate").value,
+          inProgress: form.querySelector("#inProgress").value,
+          _id: candidate.workExperience._id
+        }
+      ],
+    }
+    modal.classList.toggle("show");
+    createEditModal.classList.toggle("show");
+    wipeTable();
+    editCandidate(candidate);
+  };
 }
 
-function showConfirmModal() {
-  candidate = this;
+function editCandidate(candidate) {
+  readCandidates();
+}
+
+function showConfirmModal(candidate) {
   modal.classList.toggle("show");
-  modalContent.classList.toggle("show");
-  modalContent.querySelector("#yes").onclick = function(){
+  confirmModal.classList.toggle("show");
+  confirmModal.querySelector("#yes").onclick = function(){
     modal.classList.toggle("show");
-    modalContent.classList.toggle("show");
+    confirmModal.classList.toggle("show");
     wipeTable();
     removeCandidate(candidate);
   };
@@ -92,23 +143,28 @@ function createList(candidates) {
     <td>${candidate.address}</td>
     <td>${candidate.status}</td>
     <td>${candidate.profiles.join(",")}</td>
-    <td><img src="img/Icon-visible.png" alt="Show" id="show-applications" /></td>
-    <td><img src="img/Icon-visible.png" alt="Show" id="show-profile" /></td>
-    <td><img src="img/Icon-edit.png" alt="Edit" id="edit" /></td>
-    <td><img src="img/Icon-remove.png" alt="Remove" id="remove" /></td>`;
+    <td><img src="img/Icon-visible.png" alt="Show" class="show-applications" /></td>
+    <td><img src="img/Icon-visible.png" alt="Show" class="show-profile" /></td>
+    <td><img src="img/Icon-edit.png" alt="Edit" class="edit" /></td>
+    <td><img src="img/Icon-remove.png" alt="Remove" class="remove" /></td>`;
     table.appendChild(row);
-    row.querySelector("#show-profile").addEventListener("click", showProfile.bind(candidate));
-    row.querySelector("#show-applications").addEventListener("click", showApplications.bind(candidate));
-    row.querySelector("#edit").addEventListener("click", editCandidate.bind(candidate));
-    row.querySelector("#remove").addEventListener("click", showConfirmModal.bind(candidate));
+    row.querySelector(".show-profile").addEventListener("click", () => showProfile(candidate));
+    row.querySelector(".show-applications").addEventListener("click", () => showApplications(candidate));
+    row.querySelector(".edit").addEventListener("click", () => openEditForm(candidate));
+    row.querySelector(".remove").addEventListener("click", () => showConfirmModal(candidate));
   })
 }
 
 readCandidates();
 
 const modal = document.getElementById("background-modal");
-const modalContent = document.getElementById("confirm-modal");
-modalContent.querySelector("#no").addEventListener("click", () => {
+const confirmModal = document.getElementById("confirm-modal");
+const createEditModal = document.getElementById("create-modal");
+createEditModal.querySelector("#cancel-button").addEventListener("click", () => {
   modal.classList.toggle("show")
-  modalContent.classList.toggle("show");
+  createEditModal.classList.toggle("show");
+});
+confirmModal.querySelector("#no").addEventListener("click", () => {
+  modal.classList.toggle("show")
+  confirmModal.classList.toggle("show");
 });
