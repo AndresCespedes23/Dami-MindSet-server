f1b375d3068cfbed87b85dbe143b8872dd1201a8
nicolasblasco <nicoblasco96@gmail.com>
2021-11-22 22:17:06 -0300

    MS-55: Add modal, form, title, description, createButton, confirmCreateButton, confirmRemoveButton & cancelButton variables

--- public/js/applications.js
+++ public/js/applications.js
@@ -1,2 +1,14 @@
 window.onload = function() {
+
+  //----- Variables -----//
+
+  const modal = document.getElementById("background-modal");
+  const form = document.getElementById("form");
+  const title = document.getElementById("modal-title");
+  const description = document.getElementById("modal-description");
+  const createButton = document.getElementById("create-button");
+  const confirmCreateButton = document.getElementById("confirm-create-button");
+  const confirmRemoveButton = document.getElementById("confirm-remove-button");
+  const cancelButton = document.getElementById("cancel-button");
+
 }
