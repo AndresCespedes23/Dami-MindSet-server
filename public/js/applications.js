1820a820eb73978f51ddb9aeef436c943feddb57
nicolasblasco <nicoblasco96@gmail.com>
2021-11-22 22:25:40 -0300

    MS-55: Add function to create table with all of the Applications data

--- public/js/applications.js
+++ public/js/applications.js
@@ -12,3 +12,32 @@ window.onload = function() {
   const cancelButton = document.getElementById("cancel-button");
 
 }
+
+//----- Create rows with data retrieved from Applications into table -----//
+
+async function createList(applications) {
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
+  applications.forEach(async (application) => {
+    let position = await getName(application.idPosition,"positions");
+    let candidate = await getName(application.idCandidate,"candidates");
+    //let interview = await getName(application.idInterview,"interviews");
+    let itemList = document.createElement("tr");
+    itemList.innerHTML = `<td class="application" >${application._id}</td>
+      <td>${position}</td>
+      <td>${candidate}</td>
+      <td>${application.idInterview}</td>
+      <td>${application.dateTime}</td>
+      <td>${application.status}</td>
+      <td>${application.result}</td>
+      <td><button class="remove" class="button-list"><img src="img/Icon-remove.png" alt="Remove"/></button>
+      </td>`
+    tableBody.appendChild(itemList);
+    itemList.querySelector(".application").addEventListener("click", function() {
+      infoModal(application);
+    });
+    itemList.querySelector(".remove").addEventListener("click", function() {
+      openRemoveModal(application);
+    });
+  })
+}
