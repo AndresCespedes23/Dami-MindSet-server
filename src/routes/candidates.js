3c394c6d088856542b27733f6b0fe28a47024d46
BareaMartin <martin.barea94@gmail.com>
2021-11-25 22:33:28 -0300

    MS-84: commented validations for candidates and psychologists(update)

--- src/routes/candidates.js
+++ src/routes/candidates.js
@@ -1,45 +1,45 @@
 const router = require("express").Router();
 const candidates = require("../controllers/candidates");
-const validations = require("../validations/candidates");
+// const validations = require("../validations/candidates");
 
 router.get("/", candidates.getAll);
 router.get("/:id", candidates.getById);
 router.get("/byName/:name", candidates.getByName);
 router.post(
   "/",
-  validations.requiredPersonalInfo,
-  validations.validate,
+  // validations.requiredPersonalInfo,
+  // validations.validate,
   candidates.create,
 );
 router.post(
   "/education/:id",
-  validations.requiredEducation,
-  validations.validate,
+  // validations.requiredEducation,
+  // validations.validate,
   candidates.addEducation,
 );
 router.post(
   "/workExperience/:id",
-  validations.requiredWorkExperience,
-  validations.validate,
+  // validations.requiredWorkExperience,
+  // validations.validate,
   candidates.addWorkExperience,
 );
 router.put(
   "/:id",
-  validations.requiredPersonalInfo,
-  validations.requiredOtherInformation,
-  validations.validate,
+  // validations.requiredPersonalInfo,
+  // validations.requiredOtherInformation,
+  // validations.validate,
   candidates.update,
 );
 router.put(
   "/:id/education/:educationId",
-  validations.requiredEducation,
-  validations.validate,
+  // validations.requiredEducation,
+  // validations.validate,
   candidates.updateEducation,
 );
 router.put(
   "/:id/workExperience/:workExperienceId",
-  validations.requiredWorkExperience,
-  validations.validate,
+  // validations.requiredWorkExperience,
+  // validations.validate,
   candidates.updateWorkExperience,
 );
 router.delete("/:id", candidates.remove);
