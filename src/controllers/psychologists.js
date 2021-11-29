be5b37acaaa075b0f2e8a95c346289296c1683bf
BareaMartin <martin.barea94@gmail.com>
2021-11-29 16:37:56 -0300

    MS-96: Changed status to be set by the user

--- src/controllers/psychologists.js
+++ src/controllers/psychologists.js
@@ -8,7 +8,7 @@ const create = (req, res) => {
     password: req.body.password,
     phoneNumber: req.body.phoneNumber,
     enrollmentNumber: req.body.enrollmentNumber,
-    status: true,
+    status: req.body.status,
   };
   if (req.body.dayRange) newPsychologist.dayRange = req.body.dayRange;
   if (req.body.timeRange) newPsychologist.timeRange = req.body.timeRange;
@@ -38,7 +38,7 @@ const update = (req, res) => {
         res.status(400).json(error);
       }
       return res.status(200).json(newPsychologist);
-    },
+    }
   );
 };
 
