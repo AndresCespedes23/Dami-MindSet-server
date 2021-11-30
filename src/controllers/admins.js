1e65a93feab2502e731312a249223af4c144fc27
BareaMartin <martin.barea94@gmail.com>
2021-11-29 22:32:14 -0300

    MS-98: Added remove and create

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -17,6 +17,12 @@ const getByName = (req, res) => {
     .catch((err) => res.status(404).json(err));
 };
 
+const remove = (req, res) => {
+  Admins.findByIdAndRemove(req.params.id)
+    .then((removeAdmin) => res.status(200).json(removeAdmin))
+    .catch((error) => res.status(400).json(error));
+};
+
 const update = (req, res) => {
   const { id } = req.params;
   Admins.findByIdAndUpdate(id, req.body, { new: true })
@@ -27,9 +33,23 @@ const update = (req, res) => {
     .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
 };
 
+const create = (req, res) => {
+  const newAdmin = {
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  };
+  Admins.create(newAdmin)
+    .then((adminNew) => res.status(201).json(adminNew))
+    .catch((error) => res.status(400).json(error));
+};
+
 module.exports = {
   getAll,
   getById,
   getByName,
   update,
+  remove,
+  create,
 };
