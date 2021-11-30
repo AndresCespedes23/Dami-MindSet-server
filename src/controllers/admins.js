8358f5e128ffee0473d4738def7715b855dc0a2b
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-29 21:21:46 -0300

    MS-96: fix update method

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -18,22 +18,13 @@ const getByName = (req, res) => {
 };
 
 const update = (req, res) => {
-  const newAdmin = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    username: req.body.username,
-    isSuperAdmin: req.body.isSuperAdmin,
-  };
-  Admins.findByIdAndUpdate(req.params.id, newAdmin, (err, updatedAdmin) => {
-    if (!updatedAdmin) {
-      return res
-        .status(404)
-        .json({ msg: `Admin with id: ${req.params.id} was not found.` });
-    }
-    if (err) return res.status(400).json(err);
-    return res.status(200).json(updatedAdmin);
-  });
+  const { id } = req.params;
+  Admins.findByIdAndUpdate(id, req.body, { new: true })
+    .then((data) => {
+      if (!data) return res.status(404).json({ msg: `Admin with id: ${req.params.id} was not found.` });
+      return res.status(200).json(data);
+    })
+    .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
 };
 
 module.exports = {
