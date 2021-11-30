4a3e40b537c1e9c054221eb211e65fcc1b5dbb83
BareaMartin <martin.barea94@gmail.com>
2021-11-29 22:33:18 -0300

    MS-98: added routes for post and delete methods

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -6,5 +6,7 @@ router.get("/", admins.getAll);
 router.get("/:id", admins.getById);
 router.get("/byName/:name", admins.getByName);
 router.put("/:id", validations.required, admins.update);
+router.post("/", admins.create);
+router.delete("/:id", admins.remove);
 
 module.exports = router;
