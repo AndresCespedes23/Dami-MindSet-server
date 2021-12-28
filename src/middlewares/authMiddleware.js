eeda048db6509e759ba220f2276d81d7e0f73671
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-28 20:22:13 -0300

    MS-190: fix authMiddleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,19 +1,19 @@
 const firebase = require("../helper/firebase");
 
-const authMiddleware = async (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const { token } = req.headers;
   if (!token) {
-    return res.status(400).json({ message: "Provide a token" });
+    return res.status(400)
+      .json({ message: "Provide a token" });
   }
-  return firebase
-    .auth()
-    .verifyIdToken(token)
+  return firebase.auth().verifyIdToken(token)
     .then(() => {
       next();
     })
     .catch((error) => {
-      res.status(401).json({ message: error.toString() });
+      res.status(401)
+        .json({ message: error.toString() });
     });
 };
 
-module.export = authMiddleware;
+module.exports = authMiddleware;
