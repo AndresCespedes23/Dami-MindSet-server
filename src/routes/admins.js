9057d0c6b88a0c3f62810ea1db4fe2addb377010
Julian Vicente <julian.vicente97s@gmail.com>
2021-12-29 10:11:17 -0300

    MS-190: add authMiddleware to admin getAll route

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -3,7 +3,7 @@ const admins = require("../controllers/admins");
 const validations = require("../validations/admins");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.get("/", admins.getAll);
+router.get("/", authMiddleware, admins.getAll);
 router.get("/search", authMiddleware, admins.search);
 router.get("/:id", authMiddleware, admins.getById);
 router.put("/:id", authMiddleware, validations.required, admins.update);
